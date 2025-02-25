a8cc5e2a371f110f83e4767ed4aff009834ebc28
Carla <64254309+CarlitaCPH@users.noreply.github.com>
2025-02-25 18:37:07 +0100

    Update weightedMeanLinearRegression.py

--- weightedMeanLinearRegression.py
+++ weightedMeanLinearRegression.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Define the filename
-filename = '/Users/carlasoleta/Library/CloudStorage/GoogleDrive-carla.soleta@gmail.com/My Drive/Thesis files/CDR_data_Oct_17_2024.csv'
+filename = 'Thesis files/CDR_data_Oct_17_2024.csv'
 
 # Read the CSV file into a pandas DataFrame
 try:

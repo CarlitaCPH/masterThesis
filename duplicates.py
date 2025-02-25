5c1bac84984ec982f50021c5cf83d721a8124efe
Carla <64254309+CarlitaCPH@users.noreply.github.com>
2025-02-25 18:31:09 +0100

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Define the filename
-filename = '/Users/carlasoleta/Library/CloudStorage/GoogleDrive-carla.soleta@gmail.com/My Drive/Thesis files/CDR_data_Oct_17_2024.csv'
+filename = 'Thesis files/CDR_data_Oct_17_2024.csv'
 
 # Read the CSV file into a pandas DataFrame
 try:
